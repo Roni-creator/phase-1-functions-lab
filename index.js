ada447420b323eb07bbe3ab211f40ad9f7fe2101
veronicah <mbuviveronicah3@gmail.com>
2023-03-14 15:58:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+function  distanceFromHqInBlocks(distInblocks)
+{
+const headquatersInBlock = 42
+if (distInblocks > headquatersInBlock)
+{
+    return distInblocks - headquatersInBlock
+}
+else {return headquatersInBlock-distInblocks
+}
+
+}
+
+function distanceFromHqInFeet(feet)
+{
+  const x = distanceFromHqInBlocks(feet)*264
+  return x;
+}
+
+function distanceTravelledInFeet(start,destination){
+let x
+    if( destination>start){
+     x=(destination - start)*264
+    return x}
+    else 
+    {x=(start-destination)*264
+    return x;
+    }
+ }
+
+function calculatesFarePrice(start, destination){
+    let x =distanceTravelledInFeet(start,destination)
+ if(x <=400){
+ return 0;}
+ else if(x < 2000) {
+    return (x -400)*0.02;}
+    else if(x <2500){
+    return 25;}
+        else 
+        {return 'cannot travel that far'
+    }
+}
+
+
+ 
